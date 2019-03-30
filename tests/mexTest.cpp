97d25ae9f744f19a7d19214ad6193ccc248fc838
akira2357 <martino.n.bernasconi@gmail.com>
2019-03-30 15:28:26 +0100

    BookConstructor First and new testing methodology

--- tests/mexTest.cpp
+++ tests/mexTest.cpp
@@ -2,9 +2,8 @@
 #include "../c++/Message.hpp"
 #include "../c++/Reader.hpp"
 
-
 int main(){
-    Reader r("mexTest.csv","LFC     ");
+    Reader r("messagesTest.csv","LFC     ");
     while(!r.eof()){
         Message msg = r.createMessage();
         msg.print();
