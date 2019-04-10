ce957a534a25957af040513d54bd899bb05960ae
luigif9 <luigi.fusco@student.ecp.fr>
2019-04-11 00:05:40 +0200

    Fixed conversion to user-defined type

--- c++/utility.cpp
+++ c++/utility.cpp
@@ -5,3 +5,26 @@ id_type ID_DEFAULT = -1;
 price_type PRICE_DEFAULT = -1;
 size_type SIZE_DEFAULT = -1;
 
+long stringConvert(std::string input, the_type<long> ){
+    long ret=std::stol(input);
+    //std::cout<<"It's a long";
+    return ret;
+}
+
+double stringConvert(std::string input, the_type<double> ){
+    double ret=std::stod(input);
+    //std::cout<<"It's a double";
+    return ret;
+}
+
+long long stringConvert(std::string input, the_type<long long> ){
+    long long ret=std::stoll(input);
+    //std::cout<<"It's a long long";
+    return ret;
+}
+
+float stringConvert(std::string input, the_type<float> ){
+    float ret=std::stof(input);
+    //std::cout<<"It's a float";
+    return ret;
+}
