228c7bd8c201788b0047cb4a7c70f8fdc98fe71a
akira2357 <martino.n.bernasconi@gmail.com>
2019-04-11 19:05:03 +0200

    final commit of Enhance Parser

--- parser/utility.hpp
+++ parser/utility.hpp
@@ -15,30 +15,4 @@ uint32_t parse_uint32(char * a);
 uint64_t parse_uint64(char * a);
 uint64_t parse_ts(char * a);
 
-
-#define parse_stock(n) \
-for (i=0; i<8; i++) { \
-    stock[i] = m[i+(n)]; \
-}
-
-#define parse_issue_subtype(n) \
-for (i=0; i<2; i++) { \
-    issue_subtype[i] = m[i+(n)]; \
-}
-
-#define parse_reason(n) \
-for (i=0; i<4; i++) { \
-    reason[i] = m[i+(n)]; \
-}
-
-#define parse_mpid(n) \
-for (i=0; i<4; i++) { \
-    mpid[i] = m[i+(n)]; \
-}
-
-#define parse_attribution(n) \
-for (i=0; i<4; i++) { \
-    attribution[i] = m[i+(n)]; \
-}
-
 #endif
