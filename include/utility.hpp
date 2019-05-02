e339fc5bd2010a9f1fd42051e46cfebaff44a5b1
Ozrenka <ozrenka.dragic@gmail.com>
2019-05-02 18:14:12 +0200

    .

--- include/utility.hpp
+++ include/utility.hpp
@@ -15,11 +15,6 @@ extern side_type SIDE_DEFAULT;
 extern size_type SIZE_DEFAULT;
 extern price_type PRICE_DEFAULT;
 
-long stringConvert(std::string input, the_type<long>);
-double stringConvert(std::string input, the_type<double>);
-long long stringConvert(std::string input, the_type<long long>);
-float stringConvert(std::string input, the_type<float>);
-
 uint16_t bswap_16(uint16_t value);
 uint32_t bswap_32(uint32_t value);
 uint64_t bswap_64(uint64_t value);
